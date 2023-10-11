a8a825dac0199fef86c6c09c8df53fe53baf9185
LuClams <lunelclamy@hotmail.fr>
2023-10-11 11:25:37 +0200

    ajout comments

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const host = '0.0.0.0'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const mongoose = require ("mongoose");
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+//avec une install local
+// mongoose.connect('mongodb://127.0.0.1:27017/apinode')
+mongoose.connect('mongodb://127.0.0.1:27017/apinode')
+
+//avec Docker (mongodb://nomDuContainer/NomdeLaBDD)
+// mongoose.connect('mongodb://mongo/apinode')
+
+
+//app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded());
+app.use(express.json());
+
+const postRoute = require('./routes/postRoute');
+app.use('/posts', postRoute);
+
+const commentRoute = require('./routes/commentRoute');
+app.use('/comments', commentRoute);
+
+app.listen(port,host);
